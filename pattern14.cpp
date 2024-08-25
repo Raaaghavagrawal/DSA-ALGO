8ca46a3ce97701b08d2877f656ed7fdbe92dd169
Raghav Agrawal <codeworks2u@gmail.com>
2024-08-25 18:15:24 +0530

    Add files via upload

---
+++ pattern14.cpp
@@ -0,0 +1,25 @@
+/*    ****
+       ***
+        **
+         *
+*/
+
+//PATTERN 14
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of rows"<<endl;
+    cin>>n;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<i;j++){
+            cout<<" ";
+        }
+        for(int k=i;k<=n;k++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
