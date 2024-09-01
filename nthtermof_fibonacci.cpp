9f0310fab9e9cc7368b9bb639d8ede04e5494be8
Raghav Agrawal <codeworks2u@gmail.com>
2024-09-01 12:50:06 +0530

    Update nthtermof_fibonacci.cpp

--- nthtermof_fibonacci.cpp
+++ nthtermof_fibonacci.cpp
@@ -2,10 +2,18 @@
 using namespace std;
 int fibo(int n){
     int a=0,b=1,c;
-    for(int i=2;i<n;i++){
-        c=a+b;
-        a=b;
-        b=c;
+    if(n==1){
+        return 0;
+    }
+    else if(n==2){
+        return 1;
+    }
+    else{
+        for(int i=2;i<n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
     }
     return b;
 }
