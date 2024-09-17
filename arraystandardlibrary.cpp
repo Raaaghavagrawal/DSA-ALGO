d717c646709efb0381e277f60349646ec33ea475
Raghav Agrawal <codeworks2u@gmail.com>
2024-09-17 22:36:24 +0530

    Add files via upload

---
+++ arraystandardlibrary.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <array>
+using namespace std;
+int main(){
+    array<int,4> arr={1,2,3,4};
+    int size=arr.size();
+    
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"Element at 2nd index is :" <<arr.at(2)<<endl;
+    cout<<"Empty or not"<<arr.empty()<<endl;
+    cout<<"First element"<<arr.front()<<endl;
+    cout<<"Second element"<<arr.back()<<endl;
+    return 0;
+}
